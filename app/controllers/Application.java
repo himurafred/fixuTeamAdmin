e4670f3a0ac05d6f1dca4d4c84ab0ed2fc767b68
himurafred <himurafred@gmail.com>
2012-01-25 23:32:39 +0100

    Mise en place module authenticate

---
+++ app/controllers/Application.java
@@ -0,0 +1,8 @@
+package controllers;
+
+import models.SecurityIdent;
+import play.data.validation.Required;
+
+public class Application extends ControllerParent{
+	
+}
--- app/controllers/ControllerParent.java
+++ app/controllers/ControllerParent.java
@@ -3,19 +3,20 @@ package controllers;
 import java.util.ArrayList;
 import java.util.Properties;
 
+import models.User;
 import play.Play;
+import play.data.validation.Required;
 import play.mvc.Before;
 import play.mvc.Controller;
 import play.mvc.Http;
-import play.mvc.Http.Request;
 
-public class ControllerParent extends Controller {
+public class ControllerParent extends Controller{
 	
 	//Ce champs sert seulement à avoir une meilleure compréhension lors de l'accès aux properties.
 	protected static Properties properties = Play.configuration;
 			
 	@Before 
-	private static void setCORS() {		
+	protected static void setCORS() {		
 		//if(Play.mode.isDev()){
 			Http.Header hd = new Http.Header(); 
 			hd.name = "Access-Control-Allow-Origin"; 
@@ -24,4 +25,21 @@ public class ControllerParent extends Controller {
 			Http.Response.current().headers.put("Access-Control-Allow-Origin",hd);		
 		//}
 	}	
+	
+	/**
+	 * Permet de récupérer le user courant .
+	 * @return
+	 */
+	public static User getUser(){
+		return SecurityPlugin.getUser();
+	}
+	
+	public static void logout(){
+		SecurityPlugin.logout();
+		renderJSON(true);
+	}
+	
+	public static void login(@Required String username, @Required String password){
+    	renderJSON(SecurityPlugin.authenticate(username, password));
+    }
 }
